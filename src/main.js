b80960bf434333d2849a6feb8e940727c176200c
huangguoqiang <kookokukyo@gmail.com>
2020-03-25 00:23:46 +0800

    初始化样式、一像素问题等、创建config配置文件

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import './libs/rem.js';
+import './libs/rem.js'; /* rem布局支持 */
+import './assets/reset.css'; /* 初始化基础样式 */
+import './assets/border1px.css'; /* 解决部分手机屏幕1像素带来的页面样式兼容性问题 */
 
 Vue.config.productionTip = false
 
